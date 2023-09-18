c4fb2983df52ac8c377122f03084bd5bc09853e9
thatUltimateJSDev <raihanshamil33@gmail.com>
2023-09-18 12:24:49 +0600

    section added

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -15,10 +15,15 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Category = () => {
     return (
-        <>
+        <section>
+            <SectionTitle
+            heading={"Order Online"}
+            subHeading={"From 11:00 am to 10:00 pm"}>
+            </SectionTitle>
             <Swiper
                 slidesPerView={4}
                 spaceBetween={30}
@@ -27,7 +32,7 @@ const Category = () => {
                     clickable: true,
                 }}
                 modules={[Pagination]}
-                className="mySwiper mb-24"
+                className="mySwiper mb-24 mt-12"
             >
                 <SwiperSlide><img src={slide1} alt="" />
                 </SwiperSlide>
@@ -36,7 +41,7 @@ const Category = () => {
                 <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
                 <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
             </Swiper>
-        </>
+        </section>
     );
 };
 
