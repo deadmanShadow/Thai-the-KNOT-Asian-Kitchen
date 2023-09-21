a1b630fe7b6f622489290eff2d1a37f68dd1ec3c
thatUltimateJSDev <raihanshamil33@gmail.com>
2023-09-21 07:34:23 +0600

    login modified with captcha

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import loginImage from '../../assets/login.svg'
+import React, { useEffect, useState } from 'react';
+import loginImage from '../../assets/login.svg';
+import {
+    loadCaptchaEnginge,
+    LoadCanvasTemplate,
+    validateCaptcha
+} from 'react-simple-captcha';
 
 const Login = () => {
-    const handleLogin = event => {
+    useEffect(() => {
+        loadCaptchaEnginge(6); // Initialize the reCAPTCHA engine with 6 characters
+    }, []);
+
+    const [captchaValid, setCaptchaValid] = useState(true);
+    const [captchaSuccess, setCaptchaSuccess] = useState(false); // Added success state
+
+    const handleLogin = (event) => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
-    }
+        const captchaValue = form.captcha.value;
+
+        // Validate the captcha
+        if (!validateCaptcha(captchaValue)) {
+            setCaptchaValid(false);
+            setCaptchaSuccess(false); // Reset success state on validation failure
+            return;
+        }
+
+        // Set success state on validation success
+        setCaptchaValid(true);
+        setCaptchaSuccess(true);
+
+        console.log('Email:', email);
+        console.log('Password:', password);
+        console.log('Captcha:', captchaValue);
+
+        // Continue with your login logic here
+    };
+
     return (
         <div className="flex max-w-7xl h-screen items-center mx-auto">
             <div className="w-1/2">
@@ -16,31 +46,48 @@ const Login = () => {
             </div>
             <div className="w-1/2 grid place-items-center">
                 <div className="bg-primary/5 w-full max-w-sm rounded-lg grid place-items-center p-10">
-                    <h1 className="mb-10 md:w-1/2 font-medium text-2xl">Please Login</h1>
+                    <h1 className="mb-10 md:w-1/2 font-medium text-2xl">
+                        Please Login
+                    </h1>
                     <form onSubmit={handleLogin} className="space-y-3 w-full">
                         <div className="flex flex-col items-start">
                             <label htmlFor="email">Email</label>
                             <input
                                 type="email"
                                 id="email"
-                                className="w-full rounded-md"/>
+                                className="w-full rounded-md"
+                            />
                         </div>
                         <div className="flex flex-col items-start">
                             <label htmlFor="password">Password</label>
                             <input
                                 type="password"
                                 id="password"
-                                className="w-full rounded-md"/>
+                                className="w-full rounded-md"
+                            />
+                        </div>
+                        <div className={`flex flex-col items-start ${!captchaValid ? 'text-red-500' : ''}`}>
+                            <label htmlFor="captcha">Captcha</label>
+                            <LoadCanvasTemplate />
+                            <input
+                                type="text"
+                                id="captcha"
+                                name="captcha"
+                                className={`w-full rounded-md ${!captchaValid ? 'border border-red-500' : ''}`}
+                            />
+                            {!captchaValid && <p className="text-sm">Captcha validation failed. Please try again.</p>}
+                            {captchaSuccess && <p className="text-green-500 text-sm">Captcha validation successful!</p>} {/* Added success message */}
                         </div>
                         <div className="relative !mt-8">
-                            <button type="submit" className="btn btn-primary w-full">
+                            <button
+                                type="submit"
+                                className="btn btn-primary w-full"
+                            >
                                 Login
                             </button>
                         </div>
                         <div>
-                            <p>
-                                Don't have an account?{' '}
-                            </p>
+                            <p>Don't have an account? </p>
                         </div>
                     </form>
                 </div>
