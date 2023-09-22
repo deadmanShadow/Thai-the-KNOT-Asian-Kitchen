6274df146131908473872c299d8463a0fee0ef92
thatUltimateJSDev <raihanshamil33@gmail.com>
2023-09-22 19:14:11 +0600

    navbar modified

--- src/pages/Home/Home/Shared/NavBar/NavBar.jsx
+++ src/pages/Home/Home/Shared/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import logoImg from '../../../../../assets/logoImg.png'
 import { useContext } from "react";
 import { AuthContext } from "../../../../../Providers/AuthProvider";
+import { FaShoppingCart } from 'react-icons/fa';
 
 const NavBar = () => {
     const { user, logOut } = useContext(AuthContext);
@@ -33,26 +34,35 @@ const NavBar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link to="/menu">Our Menu</Link></li>
-                    <li><Link to="/order">Order Food</Link></li>
-                    <li><Link to="/secret">Secret</Link></li>
+                    <button className="btn badge">
+                        <li><Link to="/menu">Our Menu</Link></li>
+                        <li><Link to="/order">Order Food</Link></li>
+                        <li><Link to="/secret">Secret</Link></li>
+                    </button>
+                    <li>
+                        <Link to="/">
+                            <button className="btn btn-sm ms-2">
+                                <FaShoppingCart />
+                                <div className="badge badge-secondary">+0</div>
+                            </button>
+                        </Link>
+                    </li>
                 </ul>
             </div>
             <div className="navbar-end flex">
                 {user ? (
                     <div className="user-info flex items-center space-x-4">
                         <span className="text-green-500 font-semibold">{user?.displayName}</span>
-                        <button onClick={handleLogOut} className="btn btn-outline btn-error">Logout</button>
+                        <button onClick={handleLogOut} className="btn btn-outline btn-sm btn-error">Logout</button>
                     </div>
                 ) : (
                     <>
-                        <Link to="/login" className="btn btn-neutral">Login</Link>
-                        <Link to="/signup" className="btn btn-outline btn-accent ms-2">Register</Link>
+                        <Link to="/login" className="btn btn-neutral btn-sm">Login</Link>
+                        <Link to="/signup" className="btn btn-outline btn-accent btn-sm ms-2">Register</Link>
                     </>
                 )}
             </div>
         </div>
     );
 };
-
 export default NavBar;
